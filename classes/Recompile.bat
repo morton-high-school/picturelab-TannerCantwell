9f6b808aabe24308a7e75dc0ffaa0f285543e564
TannerCantwell <tanner.cantwell@mcusd709.org>
2025-02-10 08:46:14 -0600

    figuring stuff out idek

---
+++ classes/Recompile.bat
@@ -0,0 +1,43 @@
+@ECHO OFF
+
+ECHO Autograder for Picture Class
+
+del IntArrayWorkerTester.class
+
+del PictureFrame.class
+
+del Picture.class
+
+del PictureExplorer.class
+
+del ColorChooser.class
+
+del DigitalPicture.class
+
+del FileChooser.class
+
+del ImageDisplay.class
+
+del IntArrayWorker.class
+
+del PictureExplorer$1.class
+
+del PictureExplorer$2.class
+
+del PictureExplorer$3.class
+
+del PictureExplorer$4.class
+
+del PictureExplorer$5.class
+
+del PictureExplorer$6.class
+
+del PictureExplorer$PictureExplorerFocusTraversalPolicy.class
+
+del PictureTester.class
+
+del Pixel.class
+
+del SimplePicture.class
+
+PAUSE
